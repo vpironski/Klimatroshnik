ca65bc279c8095a1e65cb2330151455c63cfee86
vpironski <vpironski@gmail.com>
2024-09-17 22:21:31 +0300

    Initial commit

---
+++ src/main/java/com/example/Klimatroshik/KlimatroshikApplication.java
@@ -0,0 +1,13 @@
+package com.example.Klimatroshik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlimatroshikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlimatroshikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Klimatroshik/KlimatroshikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Klimatroshik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlimatroshikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
